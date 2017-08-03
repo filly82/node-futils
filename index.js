8ab6bc3cf2349292e35ac4e868a00a555ba759f1
Nahid <filly@filan.de>
2017-08-03 22:18:57 +0200

    formatDate

--- index.js
+++ index.js
@@ -18,18 +18,12 @@ module.exports = {
     {
         if (obj1 && obj2 && Object.keys(obj2).length>0)
         {
-            //iterate over all the properties in the object which is being consumed
             for (var p in obj2)
             {
-                // Property in destination object set; update its value.
                 if (obj2.hasOwnProperty(p) && typeof obj1[p] !== "undefined")
-                {
                     this.mergeObjects(obj1[p], obj2[p]);
-                } else
-                {
-                    //We don't have that level in the heirarchy so add it
+                else
                     obj1[p] = obj2[p];
-                }
             }
         }
     }
