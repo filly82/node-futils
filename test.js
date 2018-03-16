5d62f7e329b65374a5db29682484a3980eb08fa3
Nahid <filly@filan.de>
2018-03-16 17:34:29 +0100

    responseType

--- test.js
+++ test.js
@@ -2,14 +2,17 @@ var CachedRequest = require('./lib/CachedRequest');
 var cachedRequest = new CachedRequest('./test', true);
 
 
-cachedRequest.doRequest('http://www.google.de', {'duration': '3d'}).then(function(responseData)
+if (false) cachedRequest.doRequest({'url': 'https://www.filan.de', 'duration': '3d'}).then(function(responseData)
 {
     console.log(responseData);
-    console.log(Buffer.from(responseData, 'base64').toString('utf8'));
+    //console.log(Buffer.from(responseData, 'base64').toString('utf8'));
 });
 
-cachedRequest.doRequest('https://image.tmdb.org/t/p/w185/fkdFJb8ctne7C5pkoTQLpGM7Y01.jpg', {'duration': '3d', 'binary': 'true'}).then(function(responseData)
+if (true) cachedRequest.doRequest({'url': 'https://image.tmdb.org/t/p/w185/fkdFJb8ctne7C5pkoTQLpGM7Y01.jpg', 'duration': '3d', 'responseType': 'base64'}).then(function(responseData)
 {
+    //console.log(responseData);
+    //console.log(Buffer.from(responseData, 'base64').toString('binary'));
     console.log(responseData);
-    console.log(Buffer.from(responseData, 'base64').toString('binary'));
+    //var imageDataUri = require('image-data-uri');
+    //console.log(imageDataUri.encode( responseData, 'image/jpeg'));
 });
