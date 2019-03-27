d0161b67f7990889418f03b310c5f836a479e277
Nahid <filly82@filan.de>
2019-03-27 21:56:11 +0100

    export ZimbraApiClient

--- index.js
+++ index.js
@@ -5,28 +5,29 @@
 
 module.exports = {
 
-    calDavUtils: require('./lib/CalDavUtils'),
-    feedUtils: require('./lib/FeedUtils'),
-    fileUtils: require('./lib/FileUtils'),
-    formatUtils: require('./lib/FormatUtils'),
-    mailUtils: require('./lib/MailUtils'),
-    mkvManager: require('./lib/MkvManager'),
-    stringUtils: require('./lib/StringUtils'),
-    mediaUtils: require('./lib/MediaUtils'),
-    CachedRequest: require('./lib/CachedRequest'),
+   calDavUtils: require('./lib/CalDavUtils'),
+   feedUtils: require('./lib/FeedUtils'),
+   fileUtils: require('./lib/FileUtils'),
+   formatUtils: require('./lib/FormatUtils'),
+   mailUtils: require('./lib/MailUtils'),
+   mkvManager: require('./lib/MkvManager'),
+   stringUtils: require('./lib/StringUtils'),
+   mediaUtils: require('./lib/MediaUtils'),
+   CachedRequest: require('./lib/CachedRequest'),
+   ZimbraApiClient: require('./lib/ZimbraApiClient'),
 
 
-    mergeObjects: function (obj1, obj2)
-    {
-        if (obj1 && obj2 && Object.keys(obj2).length>0)
-        {
-            for (var p in obj2)
-            {
-                if (obj2.hasOwnProperty(p) && typeof obj1[p] !== "undefined")
-                    this.mergeObjects(obj1[p], obj2[p]);
-                else
-                    obj1[p] = obj2[p];
-            }
-        }
-    }
+   mergeObjects: function (obj1, obj2)
+   {
+      if (obj1 && obj2 && Object.keys(obj2).length > 0)
+      {
+         for (var p in obj2)
+         {
+            if (obj2.hasOwnProperty(p) && typeof obj1[p] !== "undefined")
+               this.mergeObjects(obj1[p], obj2[p]);
+            else
+               obj1[p] = obj2[p];
+         }
+      }
+   }
 };
